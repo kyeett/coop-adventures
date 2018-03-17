cb52e6ae2e383d74a4f91e7b8b8c46755ca6cd2e
Magnus Wahlstrand <magnus.wahlstrand@gmail.com>
2018-03-17 15:18:39 +0100

    Working example with socketio and drag

--- app.py
+++ app.py
@@ -2,16 +2,41 @@ from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
 import os
 import pyqrcode
+import logging
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['HOST'] = "0.0.0.0"
 
+socket = SocketIO(app)
 
 @app.route('/joined')
 def joined():
     return "Joined the game successfully!"
 
 
-@app.route('/qrcode')
+@socket.on('move')
+def handle_message(position):
+    logging.error(('position: ', position['x'], position['y']))
+    emit('move_broadcast', position, broadcast=True)
+
+
+@socket.on('message')
+def handle_message(message):
+    logging.error('received message: ' + message)
+
+
+@socket.on('json')
+def handle_json(json):
+    logging.error('received message: ' + str(json))
+
+
+@socket.on('connected')
+def handle_my_custom_event(json):
+    logging.error('received message: ' + str(json))
+
+
+@app.route('/')
 def qr():
 
 
@@ -24,10 +49,10 @@ def qr():
     return render_template('qrcode.html')
 
 
-@app.route('/')
-def routing():
-    return "Foo is whenever I'm with you" + request.host
+@app.route('/touch')
+def touch():
+    return render_template('touch_movement.html')
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=False)
+    socket.run(app)
